0332857832c5e3424b588739e8b0285c01a16db7
JoaquinRBP <52896009+JoaquinRBP@users.noreply.github.com>
2019-07-16 08:30:37 -0500

    Update test.php

--- Envia/test.php
+++ Envia/test.php
@@ -1,23 +1,31 @@
 
 	<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-	$longi="Hola".$_POST['longitud'];
-	$lati="Hola1".$_POST['latitud'];
-    $connectionInfo = array("UID" => "proyearqui@arquitectura", "pwd" => "Guerron123", "Database" => "gps", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:arquitectura.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-	
-	$sql="INSERT INTO prueba(id,contenido) values ('$longi','$lati');";
+if(!empty($_GET['latitud']) && !empty($_GET['longitud'])){
+	try{
+	$latitud = $_GET ['latitud'];
+	$longitud = $_GET ['longitud'];
+	/*$conexion= new PDO("mysql:host=localhost;dbname={basededatos}}","root","");
+	$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$resultado = $conexion->exec("INSERT INTO prueba (latitud,longitud) VALUES ('$latitud','$longitud');");
+	*/
+	$connectionInfo = array("UID" => "proyearqui@arquitectura", "pwd" => "Guerron123", "Database" => "gps", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:arquitectura.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	$sql="INSERT INTO prueba(id,contenido) values ('$latitud','$longitud')";
 	$resultado=sqlsrv_query($conn,$sql);
 	
-	echo "La longitud es: ".$longi;
-	echo "La latitud es: ".$lati;
-	echo "Datos recogidos correctamente";	
+	if($resultado==false){
+		echo "Datos no ingresados";
+	}
+	else{
+		echo "Datos ingresados correctamente.";	
+	}
+	}
+	catch(Exception $e){
+		die("Error: ".$e->getMessage());
+	}
 }
-catch (Exception $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+else{
+	echo "Datos vacios";
 }
-
 ?>

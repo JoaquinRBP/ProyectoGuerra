0a4a595d4d09a563085fa571a303d87b98169a25
JoaquinRBP <52896009+JoaquinRBP@users.noreply.github.com>
2019-07-16 08:27:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Incluir</title>
-</head>
-<body>
-	<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $connectionInfo = array("UID" => "proyearqui@arquitectura", "pwd" => "Guerron123", "Database" => "gps", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:arquitectura.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-	$datos="Holasdsef".$_GET['chipid'];
-	$datos1="Azure".$_GET['temperatura'];
-	$sql="INSERT INTO prueba(id,contenido) values ('$datos','$datos1');";
+<?php
+if(!empty($_GET['latitud']) && !empty($_GET['longitud'])){
+	try{
+	$latitud = $_GET ['latitud'];
+	$longitud = $_GET ['longitud'];
+	/*$conexion= new PDO("mysql:host=localhost;dbname={basededatos}}","root","");
+	$conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$resultado = $conexion->exec("INSERT INTO prueba (latitud,longitud) VALUES ('$latitud','$longitud');");
+	*/
+	$connectionInfo = array("UID" => "proyearqui@arquitectura", "pwd" => "Guerron123", "Database" => "gps", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:arquitectura.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	$sql="INSERT INTO prueba(id,contenido) values ('$latitud','$longitud')";
 	$resultado=sqlsrv_query($conn,$sql);
 	
-	$longi=$_POST['longitud'];
-	$lati=$_POST['latitud'];
-	echo "La longitud es: ".$longi;
-	echo "La latitud es: ".$lati;
-	echo "Datos recogidos correctamente";	
+	if($resultado==false){
+		echo "Datos no ingresados";
+	}
+	else{
+		echo "Datos ingresados correctamente.";	
+	}
+	}
+	catch(Exception $e){
+		die("Error: ".$e->getMessage());
+	}
 }
-catch (Exception $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
+else{
+	echo "Datos vacios";
 }
-
 ?>
-	
-</body>
-</html>
